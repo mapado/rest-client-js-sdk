876dac9d9ac326cf29461c263edf23e6bc1165ee
Julien Deniau <julien.deniau@gmail.com>
2016-06-29 13:39:58 +0200

    build module

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,7 @@
-Object.defineProperty(exports,"__esModule",{value:true});exports.AbstractClient=exports.AccessDeniedError=exports.OauthClient=undefined;var _MapadoSdk=require('./MapadoSdk');var _MapadoSdk2=_interopRequireDefault(_MapadoSdk);
+Object.defineProperty(exports,"__esModule",{value:true});exports.AbstractClient=exports.AccessDeniedError=exports.OauthClient=undefined;var _RestClientSdk=require('./RestClientSdk');var _RestClientSdk2=_interopRequireDefault(_RestClientSdk);
 var _Error=require('./Error');
 var _AbstractClient=require('./client/AbstractClient');var _AbstractClient2=_interopRequireDefault(_AbstractClient);
-var _OauthClient=require('./OauthClient');var _OauthClient2=_interopRequireDefault(_OauthClient);function _interopRequireDefault(obj){return obj&&obj.__esModule?obj:{default:obj};}exports.default=_MapadoSdk2.default;exports.
+var _OauthClient=require('./OauthClient');var _OauthClient2=_interopRequireDefault(_OauthClient);function _interopRequireDefault(obj){return obj&&obj.__esModule?obj:{default:obj};}exports.default=_RestClientSdk2.default;exports.
 
 
 
