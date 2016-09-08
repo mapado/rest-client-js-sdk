b5074acd14c7d8c818941e081452f67b4692a596
Thomas di Luccio <thomas.diluccio@mapado.com>
2016-09-08 15:09:44 +0200

    build module

--- dist/TokenGenerator/AbstractTokenGenerator.js
+++ dist/TokenGenerator/AbstractTokenGenerator.js
@@ -1,37 +1,35 @@
-Object.defineProperty(exports,"__esModule",{value:true});var _createClass=function(){function defineProperties(target,props){for(var i=0;i<props.length;i++){var descriptor=props[i];descriptor.enumerable=descriptor.enumerable||false;descriptor.configurable=true;if("value"in descriptor)descriptor.writable=true;Object.defineProperty(target,descriptor.key,descriptor);}}return function(Constructor,protoProps,staticProps){if(protoProps)defineProperties(Constructor.prototype,protoProps);if(staticProps)defineProperties(Constructor,staticProps);return Constructor;};}();var _urijs=require('urijs');var _urijs2=_interopRequireDefault(_urijs);function _interopRequireDefault(obj){return obj&&obj.__esModule?obj:{default:obj};}function _classCallCheck(instance,Constructor){if(!(instance instanceof Constructor)){throw new TypeError("Cannot call a class as a function");}}var
-
-AbstractTokenGenerator=function(){
+Object.defineProperty(exports,"__esModule",{value:true});var _createClass=function(){function defineProperties(target,props){for(var i=0;i<props.length;i++){var descriptor=props[i];descriptor.enumerable=descriptor.enumerable||false;descriptor.configurable=true;if("value"in descriptor)descriptor.writable=true;Object.defineProperty(target,descriptor.key,descriptor);}}return function(Constructor,protoProps,staticProps){if(protoProps)defineProperties(Constructor.prototype,protoProps);if(staticProps)defineProperties(Constructor,staticProps);return Constructor;};}();function _classCallCheck(instance,Constructor){if(!(instance instanceof Constructor)){throw new TypeError("Cannot call a class as a function");}}var AbstractTokenGenerator=function(){
 function AbstractTokenGenerator(){var tokenGeneratorConfig=arguments.length<=0||arguments[0]===undefined?{}:arguments[0];_classCallCheck(this,AbstractTokenGenerator);
 this.tokenGeneratorConfig=tokenGeneratorConfig;
-
-this.checkTokenGeneratorConfig(this.tokenGeneratorConfig);}_createClass(AbstractTokenGenerator,[{key:'generateToken',value:function generateToken(
-
+this.canAutogenerateToken=false;
+this.checkTokenGeneratorConfig(this.tokenGeneratorConfig);
+}_createClass(AbstractTokenGenerator,[{key:"generateToken",value:function generateToken(
 
 parameters){
-throw new Error('AbstractTokenGenerator::generateToken can not be called directly.\n                    You must implement "generateToken" method.');}},{key:'refreshToken',value:function refreshToken(
+throw new Error("AbstractTokenGenerator::generateToken can not be called directly.\n                    You must implement \"generateToken\" method.");
 
-
+}},{key:"refreshToken",value:function refreshToken(
 
 accessToken,parameters){
-throw new Error('AbstractTokenGenerator::refreshToken can not be called directly.\n                    You must implement "refreshToken" method.');}},{key:'checkTokenGeneratorConfig',value:function checkTokenGeneratorConfig(
+throw new Error("AbstractTokenGenerator::refreshToken can not be called directly.\n                    You must implement \"refreshToken\" method.");
 
-
+}},{key:"checkTokenGeneratorConfig",value:function checkTokenGeneratorConfig(
 
 config){
-return true;}},{key:'convertMapToFormData',value:function convertMapToFormData(
-
+return true;
+}},{key:"convertMapToFormData",value:function convertMapToFormData(
 
 parameters){
 var keys=Object.keys(parameters);
 
 var formData=new FormData();
 
-for(var _iterator=keys,_isArray=Array.isArray(_iterator),_i=0,_iterator=_isArray?_iterator:_iterator[typeof Symbol==='function'?Symbol.iterator:'@@iterator']();;){var _ref;if(_isArray){if(_i>=_iterator.length)break;_ref=_iterator[_i++];}else{_i=_iterator.next();if(_i.done)break;_ref=_i.value;}var key=_ref;
-formData.append(key,parameters[key]);}
+for(var _iterator=keys,_isArray=Array.isArray(_iterator),_i=0,_iterator=_isArray?_iterator:_iterator[typeof Symbol==="function"?Symbol.iterator:"@@iterator"]();;){var _ref;if(_isArray){if(_i>=_iterator.length)break;_ref=_iterator[_i++];}else{_i=_iterator.next();if(_i.done)break;_ref=_i.value;}var key=_ref;
+formData.append(key,parameters[key]);
+}
 
-
-return formData;}}]);return AbstractTokenGenerator;}();exports.default=
-
+return formData;
+}}]);return AbstractTokenGenerator;}();exports.default=
 
 
 AbstractTokenGenerator;
