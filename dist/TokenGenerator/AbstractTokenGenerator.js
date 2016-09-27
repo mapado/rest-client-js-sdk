fd843a20f947296cff1880728dbc7978ac3bc6a5
Julien Deniau <julien.deniau@gmail.com>
2016-09-27 11:14:36 +0200

    build module

--- dist/TokenGenerator/AbstractTokenGenerator.js
+++ dist/TokenGenerator/AbstractTokenGenerator.js
@@ -2,22 +2,22 @@ Object.defineProperty(exports,"__esModule",{value:true});var _createClass=functi
 function AbstractTokenGenerator(){var tokenGeneratorConfig=arguments.length<=0||arguments[0]===undefined?{}:arguments[0];_classCallCheck(this,AbstractTokenGenerator);
 this.tokenGeneratorConfig=tokenGeneratorConfig;
 this.canAutogenerateToken=false;
-this.checkTokenGeneratorConfig(this.tokenGeneratorConfig);
-}_createClass(AbstractTokenGenerator,[{key:"generateToken",value:function generateToken(
+this.checkTokenGeneratorConfig(this.tokenGeneratorConfig);}_createClass(AbstractTokenGenerator,[{key:"generateToken",value:function generateToken(
+
 
 parameters){
-throw new Error("AbstractTokenGenerator::generateToken can not be called directly.\n                    You must implement \"generateToken\" method.");
+throw new Error("AbstractTokenGenerator::generateToken can not be called directly.\n                    You must implement \"generateToken\" method.");}},{key:"refreshToken",value:function refreshToken(
 
-}},{key:"refreshToken",value:function refreshToken(
+
 
 accessToken,parameters){
-throw new Error("AbstractTokenGenerator::refreshToken can not be called directly.\n                    You must implement \"refreshToken\" method.");
+throw new Error("AbstractTokenGenerator::refreshToken can not be called directly.\n                    You must implement \"refreshToken\" method.");}},{key:"checkTokenGeneratorConfig",value:function checkTokenGeneratorConfig(
 
-}},{key:"checkTokenGeneratorConfig",value:function checkTokenGeneratorConfig(
+
 
 config){
-return true;
-}},{key:"convertMapToFormData",value:function convertMapToFormData(
+return true;}},{key:"convertMapToFormData",value:function convertMapToFormData(
+
 
 parameters){
 var keys=Object.keys(parameters);
@@ -25,11 +25,11 @@ var keys=Object.keys(parameters);
 var formData=new FormData();
 
 for(var _iterator=keys,_isArray=Array.isArray(_iterator),_i=0,_iterator=_isArray?_iterator:_iterator[typeof Symbol==="function"?Symbol.iterator:"@@iterator"]();;){var _ref;if(_isArray){if(_i>=_iterator.length)break;_ref=_iterator[_i++];}else{_i=_iterator.next();if(_i.done)break;_ref=_i.value;}var key=_ref;
-formData.append(key,parameters[key]);
-}
+formData.append(key,parameters[key]);}
 
-return formData;
-}}]);return AbstractTokenGenerator;}();exports.default=
+
+return formData;}}]);return AbstractTokenGenerator;}();exports.default=
+
 
 
 AbstractTokenGenerator;
--- dist/TokenStorage.js
+++ dist/TokenStorage.js
@@ -4,45 +4,45 @@ TokenStorage=function(){
 function TokenStorage(tokenGenerator,asyncStorage){_classCallCheck(this,TokenStorage);
 this._tokenGenerator=tokenGenerator;
 this._hasATokenBeenGenerated=false;
-this.setAsyncStorage(asyncStorage);
-}_createClass(TokenStorage,[{key:'setAsyncStorage',value:function setAsyncStorage(
+this.setAsyncStorage(asyncStorage);}_createClass(TokenStorage,[{key:'setAsyncStorage',value:function setAsyncStorage(
+
 
 asyncStorage){
-this._asyncStorage=asyncStorage;
-}},{key:'hasAccessToken',value:function hasAccessToken()
+this._asyncStorage=asyncStorage;}},{key:'hasAccessToken',value:function hasAccessToken()
+
 
 {
 return this._asyncStorage.getItem(ACCESS_TOKEN_KEY).
-then(function(accessToken){return!!accessToken;});
-}},{key:'getAccessToken',value:function getAccessToken()
+then(function(accessToken){return!!accessToken;});}},{key:'getAccessToken',value:function getAccessToken()
+
 
 {var _this=this;
 if(!this._hasATokenBeenGenerated&&!this._tokenGenerator.canAutogenerateToken){
-throw new Error('No token has been generated yet.');
-}
+throw new Error('No token has been generated yet.');}
+
 if(!this._hasATokenBeenGenerated&&this._tokenGenerator.canAutogenerateToken){
 return this._tokenGenerator.generateToken().
 then(function(){return _this._asyncStorage.getItem(ACCESS_TOKEN_KEY);}).
-then(function(token){return token&&JSON.parse(token).access_token;});
+then(function(token){return token&&JSON.parse(token).access_token;});}
 
-}
+
 
 return this._asyncStorage.getItem(ACCESS_TOKEN_KEY).
-then(function(token){return token&&JSON.parse(token).access_token;});
+then(function(token){return token&&JSON.parse(token).access_token;});}},{key:'logout',value:function logout()
 
-}},{key:'logout',value:function logout()
+
 
 {
-return this._asyncStorage.removeItem(ACCESS_TOKEN_KEY);
-}},{key:'generateToken',value:function generateToken(
+return this._asyncStorage.removeItem(ACCESS_TOKEN_KEY);}},{key:'generateToken',value:function generateToken(
+
 
 parameters){var _this2=this;
 this._hasATokenBeenGenerated=true;
 return this._tokenGenerator.generateToken(parameters).
 then(function(responseData){return _this2._storeAccessToken(responseData).
-then(function(){return responseData;});});
+then(function(){return responseData;});});}},{key:'refreshToken',value:function refreshToken(
 
-}},{key:'refreshToken',value:function refreshToken(
+
 
 parameters){var _this3=this;
 return this._asyncStorage.getItem(ACCESS_TOKEN_KEY).
@@ -51,17 +51,17 @@ _this3._tokenGenerator.
 refreshToken(JSON.parse(token),parameters).
 then(function(responseData){return(
 _this3._storeAccessToken(responseData).
-then(function(){return responseData;}));}));});
+then(function(){return responseData;}));}));});}},{key:'_storeAccessToken',value:function _storeAccessToken(
 
 
 
-}},{key:'_storeAccessToken',value:function _storeAccessToken(
+
 
 responseData){
 return this._asyncStorage.
-setItem(ACCESS_TOKEN_KEY,JSON.stringify(responseData));
+setItem(ACCESS_TOKEN_KEY,JSON.stringify(responseData));}}]);return TokenStorage;}();exports.default=
 
-}}]);return TokenStorage;}();exports.default=
+
 
 
 TokenStorage;
