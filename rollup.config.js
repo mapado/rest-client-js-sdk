4f539f61ad6503d11f3f4de58a57b68e1c517fc1
Julien Deniau <julien.deniau@gmail.com>
2017-04-19 15:06:40 +0200

    use rollup for bundling (#20)
    * update deps
    
    * include rollup as dependency
    
    * rollup configuration
    
    * use babel-preset-latest instead of es2015
    
    * upgrade yarn
    
    * remove immutable from rollup
    
    * improve UPGRADE.md

---
+++ rollup.config.js
@@ -0,0 +1,31 @@
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import babel from 'rollup-plugin-babel';
+
+const pkg = require('./package.json');
+
+export default {
+  entry: 'src/index.js',
+  exports: 'named',
+  moduleName: 'rest-client-sdk',
+  plugins: [
+    commonjs(),
+    resolve(),
+    babel({
+      exclude: 'node_modules/**',
+      babelrc: false,
+      presets: [
+        ['latest', {
+          es2015: {
+            modules: false,
+          },
+        }],
+      ],
+      plugins: ['external-helpers'],
+    }),
+  ],
+  targets: [
+    { dest: pkg.main, format: 'umd' },
+    { dest: pkg.module, format: 'es' },
+  ],
+};
